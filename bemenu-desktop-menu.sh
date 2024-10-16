411926fd178b5e36b4a36a01dc815371835a653f
Tomasz Kapias <tomasz@tkapias.net>
2024-10-17 00:02:47 +0700

    Add: cache

--- bemenu-desktop-menu.sh
+++ bemenu-desktop-menu.sh
@@ -22,7 +22,8 @@ shopt -s extglob
 declare -f format_entries
 declare -a bemenu_cmd desktop_path desktop_files
 declare AudioVideo Audio Video Development Education Game Graphics Network \
-  Office Science Settings System Utility Other tmplist
+  Office Science Settings System Utility Other
+declare tmp_list tmp_entries tmp_last old_last new_last
 
 # bemenu command
 bemenu_cmd=(bemenu)
@@ -36,56 +37,68 @@ desktop_path+=( "/usr/share/applications" )
 # shellcheck disable=SC2206
 desktop_files=( ${desktop_path[*]/%//*.desktop} )
 
-# temporary fle to store unique entires filenames
-tmplist="/tmp/.bemenu-desktop-menu.tmp"
+# tmp file storing the bemenu list generated last time
+tmp_list="/tmp/bemenu-desktop-menu_list"
+# tmp file storing filenames of unique entires
+tmp_entries="/tmp/bemenu-desktop-menu_entries"
+# tmp file storing POSIX date of the last modification to entries
+tmp_last="/tmp/bemenu-desktop-menu_last"
 
-# main categories icons
-AudioVideo="🎬"
-Audio="🎧"
-Video="🎞️"
-Development="🚀"
-Education="🎓"
-Game="🎮"
-Graphics="🎨"
-Network="📡"
-Office="💼"
-Science="🧪"
-Settings="🪛"
-System="💻"
-Utility="🛠️"
-Other="❓"
+old_last=$(cat $tmp_last)
+new_last=$(stat --format='%Y' "${desktop_files[@]}" | sort -n | tail -1)
+echo -n "$new_last" > "$tmp_last"
 
-format_entries() {
-  local filename category
-  # keep the most local version
-  filename=$(basename "$1")
-  if /usr/bin/grep -Eq "$filename" "$tmplist"; then
-    return
-  else
-    echo " $filename" >> "$tmplist"
-  fi
-  # respect NoDisplay & Hidden
-  if /usr/bin/grep -Eq '^NoDisplay=true$|^Hidden=true$' "$1"; then
-    return
-  fi
-  # assocate the first main category to an icon
-  category=$(printf '%s\n' "$(sed -rn -e 's/;/\n/g' -e '/^Categories=/{s/^Categories=(.*)$/\1 AudioVideo Audio Video Development Education Game Graphics Network Office Science Settings System Utility Other/p;q}' "$1")" | tr ' ' '\n' | sort | uniq -d | head -1)
-  [[ -z "$category" ]] && category="Other"
-  # line for the menu
-  printf '%s %-50s[%s]\n' "${!category}" "$(sed -rn '/^Name=/{s/^Name=(.*)$/\1/p;q}' "$1")" "$1"
-}
-
-# exports for the subshells in xargs
-export -f format_entries
-export AudioVideo Audio Video Development Education Game Graphics Network Office Science Settings System Utility Other
-export tmplist
-
-# create/clean tmplist
-truncate --size 0 "$tmplist"
-
-# main
-printf '%s\0' "${desktop_files[@]}" | xargs -0 -P 8 -I {} bash -c 'format_entries "$@"' _ {} \
-  | head -c -1 | sort --ignore-case --field-separator=' ' --key=1 --key=2 \
-  | "${bemenu_cmd[@]}" --prompt "󰣆 Desktop Menu" | awk -F '[][]' '{print $2}' \
+if [[ -f "$tmp_last" ]] && [[ "$old_last" -ge "$new_last" ]]; then
+  cat "$tmp_list" | "${bemenu_cmd[@]}" --prompt "󰣆 Desktop Menu" | awk -F '[][]' '{print $2}' \
   | xargs -I _ setsid --fork dex _ > /dev/null 2>&1 &
+else
+  # main categories icons
+  AudioVideo="🎬"
+  Audio="🎧"
+  Video="🎞️"
+  Development="🚀"
+  Education="🎓"
+  Game="🎮"
+  Graphics="🎨"
+  Network="📡"
+  Office="💼"
+  Science="🧪"
+  Settings="🪛"
+  System="💻"
+  Utility="🛠️"
+  Other="❓"
 
+  format_entries() {
+    local filename category
+    # keep the most local version
+    filename=$(basename "$1")
+    if /usr/bin/grep -Eq "$filename" "$tmp_entries"; then
+      return
+    else
+      echo " $filename" >> "$tmp_entries"
+    fi
+    # respect NoDisplay & Hidden
+    if /usr/bin/grep -Eq '^NoDisplay=true$|^Hidden=true$' "$1"; then
+      return
+    fi
+    # assocate the first main category to an icon
+    category=$(printf '%s\n' "$(sed -rn -e 's/;/\n/g' -e '/^Categories=/{s/^Categories=(.*)$/\1 AudioVideo Audio Video Development Education Game Graphics Network Office Science Settings System Utility Other/p;q}' "$1")" | tr ' ' '\n' | sort | uniq -d | head -1)
+    [[ -z "$category" ]] && category="Other"
+    # line for the menu
+    printf '%s %-50s[%s]\n' "${!category}" "$(sed -rn '/^Name=/{s/^Name=(.*)$/\1/p;q}' "$1")" "$1"
+  }
+
+  # exports for the subshells in xargs
+  export -f format_entries
+  export AudioVideo Audio Video Development Education Game Graphics Network Office Science Settings System Utility Other
+  export tmp_entries
+
+  # create/clean tmp_entries
+  truncate --size 0 "$tmp_entries"
+
+  # main
+  printf '%s\0' "${desktop_files[@]}" | xargs -0 -P 8 -I {} bash -c 'format_entries "$@"' _ {} \
+    | head -c -1 | sort --ignore-case --field-separator=' ' --key=1 --key=2 | tee "$tmp_list" \
+    | "${bemenu_cmd[@]}" --prompt "󰣆 Desktop Menu" | awk -F '[][]' '{print $2}' \
+    | xargs -I _ setsid --fork dex _ > /dev/null 2>&1 &
+fi
