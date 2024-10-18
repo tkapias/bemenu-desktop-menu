b96eb6c6337eca40460dbfe5ac1a5a0633c438c9
Tomasz Kapias <tomasz@tkapias.net>
2024-10-18 13:39:30 +0700

    fix: tmp -> user cache

--- bemenu-desktop-menu.sh
+++ bemenu-desktop-menu.sh
@@ -37,23 +37,24 @@ desktop_path+=( "/usr/share/applications" )
 # shellcheck disable=SC2206
 desktop_files=( ${desktop_path[*]/%//*.desktop} )
 
-# tmp file storing the bemenu list generated last time
-tmp_list="/tmp/bemenu-desktop-menu_list"
-# tmp file storing filenames of unique entires
-tmp_entries="/tmp/bemenu-desktop-menu_entries"
-# tmp file storing POSIX date of the last modification and count of entries
-tmp_last="/tmp/bemenu-desktop-menu_last"
+[[ ! -d "$HOME/.cache/bemenu-desktop-menu/" ]] && /usr/bin/mkdir -p "$HOME/.cache/bemenu-desktop-menu/"
+# cache storing the bemenu list generated last time
+tmp_list="$HOME/.cache/bemenu-desktop-menu/list"
+# cache storing filenames of unique entires
+tmp_entries="$HOME/.cache/bemenu-desktop-menu/entries"
+# cache storing POSIX date of the last modification and count of entries
+tmp_last="$HOME/.cache/bemenu-desktop-menu/last"
 
-# check if the tmp needs an update
-old_last=$(cut -d " " -f1 < $tmp_last)
-new_last=$(stat --format='%Y' "${desktop_files[@]}" | sort -n | tail -1)
-old_count=$(cut -d " " -f2 < $tmp_last)
-new_count=$(/usr/bin/ls "${desktop_files[@]}" | wc -l)
+# check if the cache is up-to-date
+old_last=$(/usr/bin/cut -d " " -f1 < $tmp_last)
+new_last=$(/usr/bin/stat --format='%Y' "${desktop_files[@]}" | /usr/bin/sort -n | /usr/bin/tail -1)
+old_count=$(/usr/bin/cut -d " " -f2 < $tmp_last)
+new_count=$(/usr/bin/ls "${desktop_files[@]}" | /usr/bin/wc -l)
 echo -n "$new_last $new_count" > "$tmp_last"
 
 if [[ -f "$tmp_last" ]] && [[ "$old_last" -ge "$new_last" ]] && [[ "$old_count" == "$new_count" ]]; then
-  "${bemenu_cmd[@]}" --prompt "󰣆 Desktop Menu" < "$tmp_list" | awk -F '[][]' '{print $2}' \
-  | xargs -I _ setsid --fork dex _ > /dev/null 2>&1 &
+  "${bemenu_cmd[@]}" --prompt "󰣆 Desktop Menu" < "$tmp_list" | /usr/bin/awk -F '[][]' '{print $2}' \
+  | /usr/bin/xargs -I _ /usr/bin/setsid --fork /usr/bin/dex _ > /dev/null 2>&1 &
 else
   # main categories icons
   AudioVideo="🎬"
@@ -72,12 +73,12 @@ else
   Other="❓"
 
   # create/clean tmp_entries
-  truncate --size 0 "$tmp_entries"
+  /usr/bin/truncate --size 0 "$tmp_entries"
 
   format_entries() {
     local filename category name genericname lang
     # keep the most local version
-    filename=$(basename "$1")
+    filename=$(/usr/bin/basename "$1")
     if /usr/bin/grep -Eq "$filename" "$tmp_entries"; then
       return
     else
@@ -88,27 +89,27 @@ else
       return
     fi
     # assocate the first main category to an icon
-    category=$(printf '%s\n' "$(sed -rn -e 's/;/\n/g' -e '/^Categories=/{s/^Categories=(.*)$/\1 AudioVideo Audio Video Development Education Game Graphics Network Office Science Settings System Utility Other/p;q}' "$1")" | tr ' ' '\n' | sort | uniq -d | head -1)
+    category=$(/usr/bin/printf '%s\n' "$(/usr/bin/sed -rn -e 's/;/\n/g' -e '/^Categories=/{s/^Categories=(.*)$/\1 AudioVideo Audio Video Development Education Game Graphics Network Office Science Settings System Utility Other/p;q}' "$1")" | /usr/bin/tr ' ' '\n' | /usr/bin/sort | /usr/bin/uniq -d | /usr/bin/head -1)
     [[ -z "$category" ]] && category="Other"
     # localized names
     if [[ -n "${LANGUAGE::2}" ]]; then
       lang="\[${LANGUAGE::2}\]"
-      if grep -q -e "Name[${lang}]="; then
-        name=$(sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^Name${lang}=(.*)$/\1/p}" "$1")
+      if /usr/bin/grep -q -e "Name[${lang}]="; then
+        name=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^Name${lang}=(.*)$/\1/p}" "$1")
       else
-        name=$(sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^Name=(.*)$/\1/p}" "$1")
+        name=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^Name=(.*)$/\1/p}" "$1")
       fi
-      if grep -q -e "GenericName[${lang}]="; then
-        genericname=$(sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^GenericName${lang}=(.*)$/\1/p}" "$1")
+      if /usr/bin/grep -q -e "GenericName[${lang}]="; then
+        genericname=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^GenericName${lang}=(.*)$/\1/p}" "$1")
       else
-        genericname=$(sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^GenericName=(.*)$/\1/p}" "$1")
+        genericname=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^GenericName=(.*)$/\1/p}" "$1")
       fi
     fi
     if [[ -n "$genericname" ]] && [[ ! "$genericname" == "$name" ]]; then
       name+="  ($genericname)"
     fi
     # line for the menu
-    printf '%s %-55s[%s]\n' "${!category}" "$name" "$1"
+    /usr/bin/printf '%s %-55s[%s]\n' "${!category}" "$name" "$1"
   }
 
   # exports for the subshells in xargs
@@ -118,8 +119,8 @@ else
   export tmp_entries
 
   # main
-  printf '%s\0' "${desktop_files[@]}" | xargs -0 -P 8 -I {} bash -c 'format_entries "$@"' _ {} \
-    | head -c -1 | sort --ignore-case --field-separator=' ' --key=1 --key=2 | tee "$tmp_list" \
-    | "${bemenu_cmd[@]}" --prompt "󰣆 Desktop Menu" | awk -F '[][]' '{print $2}' \
-    | xargs -I _ setsid --fork dex _ > /dev/null 2>&1 &
+  /usr/bin/printf '%s\0' "${desktop_files[@]}" | /usr/bin/xargs -0 -P 8 -I {} bash -c 'format_entries "$@"' _ {} \
+    | /usr/bin/head -c -1 | /usr/bin/sort --ignore-case --field-separator=' ' --key=1 --key=2 | /usr/bin/tee "$tmp_list" \
+    | "${bemenu_cmd[@]}" --prompt "󰣆 Desktop Menu" | /usr/bin/awk -F '[][]' '{print $2}' \
+    | /usr/bin/xargs -I _ /usr/bin/setsid --fork /usr/bin/dex _ > /dev/null 2>&1 &
 fi
