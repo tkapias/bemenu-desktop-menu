816b5825da3be61fad1b52852ad702edc1d62f3f
Tomasz Kapias <tomasz@tkapias.net>
2024-11-01 13:13:06 +0700

    fix: fmt

--- bemenu-desktop-menu.sh
+++ bemenu-desktop-menu.sh
@@ -22,20 +22,20 @@ shopt -s extglob
 declare -f format_entries
 declare -a bemenu_cmd desktop_path desktop_files
 declare AudioVideo Audio Video Development Education Game Graphics Network \
-  Office Science Settings System Utility Other
+	Office Science Settings System Utility Other
 declare tmp_list tmp_entries tmp_last old_last new_last old_count new_count
 
 # bemenu command
 bemenu_cmd=(bemenu)
 
 # desktop entries directories paths
-desktop_path=( "$HOME/.local/share/applications" )
-desktop_path+=( "/usr/local/share/applications" )
-desktop_path+=( "/usr/share/applications" )
+desktop_path=("$HOME/.local/share/applications")
+desktop_path+=("/usr/local/share/applications")
+desktop_path+=("/usr/share/applications")
 
 # desktop entries absolute filepaths
 # shellcheck disable=SC2206
-desktop_files=( ${desktop_path[*]/%//*.desktop} )
+desktop_files=(${desktop_path[*]/%//*.desktop})
 
 [[ ! -d "$HOME/.cache/bemenu-desktop-menu/" ]] && /usr/bin/mkdir -p "$HOME/.cache/bemenu-desktop-menu/"
 # cache storing the bemenu list generated last time
@@ -46,79 +46,79 @@ tmp_entries="$HOME/.cache/bemenu-desktop-menu/entries"
 tmp_last="$HOME/.cache/bemenu-desktop-menu/last"
 
 # check if the cache is up-to-date
-old_last=$(/usr/bin/cut -d " " -f1 2> /dev/null < "$tmp_last" || echo 0)
+old_last=$(/usr/bin/cut -d " " -f1 2>/dev/null <"$tmp_last" || echo 0)
 new_last=$(/usr/bin/stat --format='%Y' "${desktop_files[@]}" | /usr/bin/sort -n | /usr/bin/tail -1)
-old_count=$(/usr/bin/cut -d " " -f2 2> /dev/null < "$tmp_last" || echo 0)
+old_count=$(/usr/bin/cut -d " " -f2 2>/dev/null <"$tmp_last" || echo 0)
 new_count=$(/usr/bin/ls "${desktop_files[@]}" | /usr/bin/wc -l)
-echo -n "$new_last $new_count" > "$tmp_last"
+echo -n "$new_last $new_count" >"$tmp_last"
 
 if [[ -f "$tmp_last" ]] && [[ "$old_last" -ge "$new_last" ]] && [[ "$old_count" == "$new_count" ]]; then
-  "${bemenu_cmd[@]}" --prompt "󰣆 Desktop Menu" < "$tmp_list" | /usr/bin/awk -F '[][]' '{print $2}' \
-  | /usr/bin/xargs -I _ /usr/bin/setsid --fork /usr/bin/dex _ > /dev/null 2>&1 &
+	"${bemenu_cmd[@]}" --prompt "󰣆 Desktop Menu" <"$tmp_list" | /usr/bin/awk -F '[][]' '{print $2}' |
+		/usr/bin/xargs -I _ /usr/bin/setsid --fork /usr/bin/dex _ >/dev/null 2>&1 &
 else
-  # main categories icons
-  AudioVideo="🎬"
-  Audio="🎧"
-  Video="🎞️"
-  Development="🚀"
-  Education="🎓"
-  Game="🎮"
-  Graphics="🎨"
-  Network="📡"
-  Office="💼"
-  Science="🧪"
-  Settings="🪛"
-  System="💻"
-  Utility="🛠️"
-  Other="❓"
+	# main categories icons
+	AudioVideo="🎬"
+	Audio="🎧"
+	Video="🎞️"
+	Development="🚀"
+	Education="🎓"
+	Game="🎮"
+	Graphics="🎨"
+	Network="📡"
+	Office="💼"
+	Science="🧪"
+	Settings="🪛"
+	System="💻"
+	Utility="🛠️"
+	Other="❓"
 
-  # create/clean tmp_entries
-  /usr/bin/truncate --size 0 "$tmp_entries"
+	# create/clean tmp_entries
+	/usr/bin/truncate --size 0 "$tmp_entries"
 
-  format_entries() {
-    local filename category name genericname
-    # keep the most local version
-    filename=$(/usr/bin/basename "$1")
-    if /usr/bin/grep -Eq "$filename" "$tmp_entries"; then
-      return
-    else
-      echo " $filename" >> "$tmp_entries"
-    fi
-    # respect NoDisplay & Hidden
-    if /usr/bin/grep -Eq '^NoDisplay=true$|^Hidden=true$' "$1"; then
-      return
-    fi
-    # assocate the first main category to an icon
-    category=$(/usr/bin/printf '%s\n' "$(/usr/bin/sed -rn -e 's/;/\n/g' -e '/^Categories=/{s/^Categories=(.*)$/\1 AudioVideo Audio Video Development Education Game Graphics Network Office Science Settings System Utility Other/p;q}' "$1")" | /usr/bin/tr ' ' '\n' | /usr/bin/sort | /usr/bin/uniq -d | /usr/bin/head -1)
-    [[ -z "$category" ]] && category="Other"
-    # localized names
-    name=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^Name\[${LANGUAGE::2}\]=(.*)$/\1/p}" "$1")
-    if [[ -z "$name" ]]; then
-      name=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^Name=(.*)$/\1/p}" "$1")
-    fi
-    genericname=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^GenericName\[${LANGUAGE::2}\]=(.*)$/\1/p}" "$1")
-    if [[ -z "$genericname" ]]; then
-      genericname=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^GenericName=(.*)$/\1/p}" "$1")
-    fi
-    if [[ -n "$genericname" ]] && [[ ! "$genericname" == "$name" ]]; then
-      genericname=" ($genericname)"
-    else
-      genericname=""
-    fi
-    padding="                                                            "
-    # line for the menu
-    /usr/bin/printf '%s %s%s%s [%s]\n' "${!category}" "$name" "${padding:$((${#name} + ${#genericname}))}" "$genericname" "$1"
-  }
+	format_entries() {
+		local filename category name genericname
+		# keep the most local version
+		filename=$(/usr/bin/basename "$1")
+		if /usr/bin/grep -Eq "$filename" "$tmp_entries"; then
+			return
+		else
+			echo " $filename" >>"$tmp_entries"
+		fi
+		# respect NoDisplay & Hidden
+		if /usr/bin/grep -Eq '^NoDisplay=true$|^Hidden=true$' "$1"; then
+			return
+		fi
+		# assocate the first main category to an icon
+		category=$(/usr/bin/printf '%s\n' "$(/usr/bin/sed -rn -e 's/;/\n/g' -e '/^Categories=/{s/^Categories=(.*)$/\1 AudioVideo Audio Video Development Education Game Graphics Network Office Science Settings System Utility Other/p;q}' "$1")" | /usr/bin/tr ' ' '\n' | /usr/bin/sort | /usr/bin/uniq -d | /usr/bin/head -1)
+		[[ -z "$category" ]] && category="Other"
+		# localized names
+		name=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^Name\[${LANGUAGE::2}\]=(.*)$/\1/p}" "$1")
+		if [[ -z "$name" ]]; then
+			name=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^Name=(.*)$/\1/p}" "$1")
+		fi
+		genericname=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^GenericName\[${LANGUAGE::2}\]=(.*)$/\1/p}" "$1")
+		if [[ -z "$genericname" ]]; then
+			genericname=$(/usr/bin/sed -rn "/^\[Desktop Entry\]$/,/^\[/{s/^GenericName=(.*)$/\1/p}" "$1")
+		fi
+		if [[ -n "$genericname" ]] && [[ ! "$genericname" == "$name" ]]; then
+			genericname=" ($genericname)"
+		else
+			genericname=""
+		fi
+		padding="                                                            "
+		# line for the menu
+		/usr/bin/printf '%s %s%s%s [%s]\n' "${!category}" "$name" "${padding:$((${#name} + ${#genericname}))}" "$genericname" "$1"
+	}
 
-  # exports for the subshells in xargs
-  export -f format_entries
-  export AudioVideo Audio Video Development Education Game Graphics Network \
-    Office Science Settings System Utility Other
-  export tmp_entries
+	# exports for the subshells in xargs
+	export -f format_entries
+	export AudioVideo Audio Video Development Education Game Graphics Network \
+		Office Science Settings System Utility Other
+	export tmp_entries
 
-  # main
-  /usr/bin/printf '%s\0' "${desktop_files[@]}" | /usr/bin/xargs -0 -P 8 -I {} bash -c 'format_entries "$@"' _ {} \
-    | /usr/bin/head -c -1 | /usr/bin/sort --ignore-case --field-separator=' ' --key=1 --key=2 | /usr/bin/tee "$tmp_list" \
-    | "${bemenu_cmd[@]}" --prompt "󰣆 Desktop Menu" | /usr/bin/awk -F '[][]' '{print $2}' \
-    | /usr/bin/xargs -I _ /usr/bin/setsid --fork /usr/bin/dex _ > /dev/null 2>&1 &
+	# main
+	/usr/bin/printf '%s\0' "${desktop_files[@]}" | /usr/bin/xargs -0 -P 8 -I {} bash -c 'format_entries "$@"' _ {} |
+		/usr/bin/head -c -1 | /usr/bin/sort --ignore-case --field-separator=' ' --key=1 --key=2 | /usr/bin/tee "$tmp_list" |
+		"${bemenu_cmd[@]}" --prompt "󰣆 Desktop Menu" | /usr/bin/awk -F '[][]' '{print $2}' |
+		/usr/bin/xargs -I _ /usr/bin/setsid --fork /usr/bin/dex _ >/dev/null 2>&1 &
 fi
